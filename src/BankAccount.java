0ca2340989f2a0e336a8a33250d44cd67af2a122
sean-sanchez02 <sean.sanchez02@utrgv.edu>
2025-02-03 19:21:39 +0000

    almost done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,16 @@
+public class BankAccount {
+    public static void main(String[] arg) {
+        float balance = 2175.37f;
+        balance -= 302.50f;
+        balance += 50.03f;
+        balance = balance / 2f + 20.00f;
+        balance -= 1f;
+        balance *= 2f;
+        balance += 1f;
+
+
+        System.out.println("Your current balance is $" + balance);
+
+    }
+}
 
