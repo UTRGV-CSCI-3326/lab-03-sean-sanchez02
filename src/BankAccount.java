c632a33d2f8ecd80d42218c15050061c060f0aad
sean-sanchez02 <sean.sanchez02@utrgv.edu>
2025-02-04 03:26:19 +0000

    output all

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,13 +1,20 @@
 public class BankAccount {
     public static void main(String[] args) {
         float balance = 2175.37f;
+        System.out.println("Your current balance is $" + balance);
         balance -= 302.50f;
+        System.out.println("Your current balance is $" + balance);
         balance += 50.03f;
+        System.out.println("Your current balance is $" + balance);
         balance = balance / 2f + 20.00f;
+        System.out.println("Your current balance is $" + balance);
         balance -= 1f;
+        System.out.println("Your current balance is $" + balance);
         balance *= 2f;
+        System.out.println("Your current balance is $" + balance);
         balance += 1f;
 
+        System.out.println("Your current balance is $" + balance);
         System.out.println("Your current balance is $" + String.format("%.2f", balance)); //formats to decimal points only
 
     }
